b4746733ba76f85befb9720d15b34f5f2674fd39
lumirelle <shabbyacc@outlook.com>
2026-05-21 14:16:02 +0800

    feat: init project with project selection analysis

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+// @ts-check
+import { antfu } from '@antfu/eslint-config'
+import oxlint from 'eslint-plugin-oxlint'
+
+export default antfu(
+  {
+    type: 'lib',
+  },
+  ...oxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
+)
+  // If you are not using `bun`, you can remove this.
+  .override('antfu/perfectionist/setup', {
+    rules: {
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          environment: 'bun',
+          groups: [
+            'type-import',
+            ['type-parent', 'type-sibling', 'type-index', 'type-internal'],
+            'value-builtin',
+            'value-external',
+            'value-internal',
+            ['value-parent', 'value-sibling', 'value-index'],
+            'side-effect',
+            'ts-equals-import',
+            'unknown',
+          ],
+          newlinesBetween: 'ignore',
+          newlinesInside: 'ignore',
+          order: 'asc',
+          type: 'natural',
+        },
+      ],
+    },
+  })
